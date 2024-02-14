b2bf2d4b6a3e1a10f7a2a3b8782a8eefa80b1248
JaquePaula <jaquemarques.santos@gmail.com>
2024-02-14 15:02:04 -0300

    add componente Home e css

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <h1 className="titulo">Home</h1>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
+        alt="Imagem Tela Inicial"
+        className="img"
+      />
+    </>
+  );
+}
+
+export default Home;
