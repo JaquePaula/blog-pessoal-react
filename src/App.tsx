5e7f0fc8e822710a543a25b31ea8a07c07b4fbaa
JaquePaula <jaquemarques.santos@gmail.com>
2024-02-16 14:54:57 -0300

    add Props e Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,55 @@
-import React from "react";
 import Home from "./paginas/home/Home";
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <Home />;
+  const [valor, setValor] = useState(0);
+  const [completed, setCompleted] = useState(false);
+  const [tarefa, setTarefa] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (completed) {
+      setTarefa("Parabéns! Você concluiu a tarefa!");
+    }
+  }, [completed]);
+
+  function handleClick() {
+    setValor(valor + 1);
+  }
+
+  return (
+    <>
+      <Home
+        title={"Componente home"}
+        description={"Este é um componente home que recebe props"}
+      />
+
+    <h1 className="text-3xl font-bold text-green-500 underline text-center">
+      Hello world!
+    </h1> 
+
+      <p>O valor é: {valor}</p>
+      <button onClick={handleClick}>Clique aqui para adicionar 1</button>
+
+      <div>
+        <h2>Tarefa</h2>
+        <h3>{tarefa}</h3>
+        <p>Conclua a tarefa:</p>
+        <button onClick={() => setCompleted(true)}>Concluir Tarefa</button>
+      </div>
+
+      <br />
+
+      <div>
+      {loggedIn ? (
+        <h1>Bem-vindo de volta!</h1>
+      ) : (
+        <button onClick={() => setLoggedIn(true)}>Entrar</button>
+      )}
+    </div>
+    </>
+  );
 }
 
 export default App;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
 import "./Home.css";
 
-function Home() {
+interface HomeProps{
+  title: string;
+  description: string;
+}
+
+
+
+function Home({title, description}: HomeProps) {
   return (
     <>
-      <h1 className="titulo">Home</h1>
+      <h2 className="titulo">{title}</h2>
+      <p>{description}</p>
+      
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
         alt="Imagem Tela Inicial"
