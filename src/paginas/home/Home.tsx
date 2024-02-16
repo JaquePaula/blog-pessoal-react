dbab87e40983d042864ae87bcb0474df17725c6a
JaquePaula <jaquemarques.santos@gmail.com>
2024-02-16 15:45:34 -0300

    add react router dom

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,91 +1,23 @@
-import "./Home.css";
+import { useNavigate, Link } from 'react-router-dom'
 
-function Home() {
-  return (
-    <>
-   <div>
-                <div className='grid grid-cols-12 gap-2 m-4'>
+const Home = () => {
+    const navigate = useNavigate()
+    return (
 
-                    <div className='col-span-6 bg-green-300'>
-                        container 1
-                    </div>
-
-                    <div className='col-span-6 bg-yellow-200'>
-                        container 2
-                    </div>
-
-                </div>
-                <div className='grid grid-cols-12 gap-2 m-4'>
-
-                    <div className='col-span-8 bg-green-300'>
-                        container 1
-                    </div>
-
-                    <div className='col-span-4 bg-yellow-200'>
-                        container 2
-                    </div>
-
-                </div>
-
-                <div className='grid grid-cols-12 gap-2 m-4'>
-
-                    <div className='col-span-4 bg-green-300'>
-                        container 1
-                    </div>
-
-                    <div className='col-span-4 bg-yellow-200'>
-                        container 2
-                    </div>
-
-                    <div className='col-span-4 bg-red-500'>
-                        container 3
-                    </div>
-
-                </div>
-
-                <div className='grid grid-cols-12 gap-2 m-4'>
-                <div className='col-span-3 bg-green-300'>
-                        container 1
-                    </div>
-
-                    <div className='col-span-3 bg-yellow-200'>
-                        container 2
-                    </div>
-
-                    <div className='col-span-3 bg-red-500'>
-                        container 3
-                    </div>
-
-                    <div className='col-span-3 bg-blue-500'>
-                        container 4
-                    </div>
-
-                </div>
-
-                <div className='grid grid-cols-12 gap-2 m-4'>
-
-                    <div className='col-span-4 bg-green-300'>
-                        container 1
-                    </div>
-
-                    <div className='col-span-4 bg-yellow-200'>
-                        container 2
-                    </div>
-
-                    <div className='col-span-2 bg-red-500'>
-                        container 3
-                    </div>
-
-                    <div className='col-span-2 bg-blue-500'>
-                        container 4
-                    </div>
-
-                </div>
-
+        <div>
+            <h2 className="text-slate-900 text-5xl  m-4">Home</h2>
+            <div>
+                <button type='submit'
+                    className='hover:underline mx-4'
+                    onClick={() => { navigate('/login') }}>
+                    Login useNavigate
+                </button>
+                <Link to='/login' className='hover:underline mx-4'>Login por Link</Link>
             </div>
 
-    </>
-  );
+        </div>
+
+    )
 }
 
-export default Home;
+export default Home
