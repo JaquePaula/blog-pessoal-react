594429dd72e8ad44001536a8d707fbedd683d779
JaquePaula <jaquemarques.santos@gmail.com>
2024-02-19 16:51:54 -0300

    add models

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import './App.css';
-import Home from './paginas/home/Home';
-import Navbar from './componentes/navBar/NavBar';
-import Footer from './componentes/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './paginas/login/Login';
+import "./App.css";
 
+import Navbar from "./componentes/navBar/NavBar";
+import Footer from "./componentes/footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./paginas/login/Login";
+import Cadastro from "./paginas/cadastro/Cadastro";
+import Home from "./paginas/home/Home";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
-          <div className='min-h-[80vh]'>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+      </AuthProvider>
     </>
-);
+  );
 }
 export default App;
